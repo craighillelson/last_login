6036b971075db0281778b9dea079f44d6de18e93
Craig Hillelson <craighillelson@gmail.com>
2019-10-03 15:34:12 -0400

    populates dict, prints results
    Checks to see if all users have logged in within the number of days. If that's the case, alerts user to same.

--- last_login.py
+++ last_login.py
@@ -10,6 +10,9 @@ TODAY = date.today()
 NUMBER_OF_DAYS = input("This script finds users who haven't logged in in a \
 number of days. Please specify a number of days.\n")
 
+USERS_LOGINS = {}
+
+# loop through the contents csv and populate a dictionary
 with open('gg_test_data.csv', 'r') as in_csvfile:
     READER = csv.DictReader(in_csvfile)
     for row in READER:
@@ -18,7 +21,17 @@ with open('gg_test_data.csv', 'r') as in_csvfile:
         last_login_obj = datetime.strptime(last_login, '%Y-%m-%d')
         delta = date.today() - last_login_obj.date()
         if delta.days > NUMBER_OF_DAYS:
-            print RTN()
-            print "account:", email
-            print "days since last login:", delta.days
-            print RTN()
+            USERS_LOGINS[email] = last_login_obj
+        else:
+            pass
+
+# if the dictionary populated above has any values, print them
+# if not, alert user
+if(USERS_LOGINS):
+    print RTN()
+    for k, v in USERS_LOGINS.items():
+        print "user:", k
+        print "last login:", v.date()
+        print RTN()
+else:
+    print "Each user has logged in within the number of days you specified."

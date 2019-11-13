a6ffdc9ff6ac8cb2de957b49aa34bcd58b5328a7
Craig Hillelson <craighillelson@gmail.com>
2019-11-13 17:39:07 -0500

    updated output_file

--- last_login.py
+++ last_login.py
@@ -3,6 +3,17 @@
 import csv
 import datetime
 
+def write_to_csv(name_of_file, dct):
+    """ write dictionary to csv """
+    HEADERS = 'user', 'last login', 'days since last login'
+    with open(name_of_file, "w") as out_file:
+        out_csv = csv.writer(out_file)
+        out_csv.writerow(HEADERS) # define HEADERS before running function
+        for gg_user, v in dct.items(): # rename keys and values to make to make them meaningful
+            keys_values = (gg_user, v[0].date(), v[1].days)
+            out_csv.writerow(keys_values)
+
+
 RTN = lambda: "\n"
 
 NUMBER_OF_DAYS = input("This script finds users who haven't logged in in a \
@@ -28,10 +39,13 @@ with open('file.csv', 'r') as in_csvfile:
 # if not, alert user
 if USERS_LOGINS:
     print(RTN())
-    for k, v in USERS_LOGINS.items():
-        print("user:", k)
-        print("last login:", v[0].date())
-        print("days since last login:", v[1].days)
+    for user, login in USERS_LOGINS.items():
+        print(f"user: {user}")
+        print(f"last login: {login[0].date()}")
+        print(f"days since last login: {login[1].days}")
         print(RTN())
 else:
     print("Each user has logged in within the number of days you specified.")
+
+write_to_csv('output_file.csv', USERS_LOGINS)
+print("'output_file.csv' exported successfully")

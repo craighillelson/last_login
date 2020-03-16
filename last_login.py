b6f6f87249b833c0d7ba5f5aaedf9f3e689b47c7
Craig Hillelson <craighillelson@gmail.com>
2020-03-16 00:01:47 -0400

    update last_login.py
    simplified code

--- last_login.py
+++ last_login.py
@@ -1,57 +1,41 @@
 """ __doc__ """
 
 import csv
-import datetime
-
-def write_to_csv(name_of_file, dct):
-    """ write dictionary to csv """
-    HEADERS = 'user', 'last login', 'days since last login'
-    with open(name_of_file, "w") as out_file:
-        out_csv = csv.writer(out_file)
-        out_csv.writerow(HEADERS)
-        for gg_user, v in dct.items():
-            keys_values = (gg_user, v[0].date(), v[1].days)
-            out_csv.writerow(keys_values)
-
+import re
+from collections import namedtuple
+from datetime import date
+from datetime import datetime
 
 RTN = lambda: '\n'
 
-NUMBER_OF_DAYS = input('This script finds users who haven\'t logged in in a '
-                       'number of days.\nPlease specify a number of days since '
-                       'last login.\n')
+today = date.today()
 
-USERS_LOGINS = {}
+DAYS_SINCE_LAST_LOGIN = {}
 
-# loop through the contents csv and populate a dictionary
-with open('file.csv', 'r') as in_csvfile:
-    READER = csv.DictReader(in_csvfile)
-    for row in READER:
-        email = row['Email Address [Required]']
-        last_login = row['Last Sign In [READ ONLY]']
-        last_login_obj = datetime.datetime.strptime(last_login, \
-        '%m/%d/%y %H:%M')
-        delta = datetime.datetime.now() - last_login_obj
-        if delta.days > int(NUMBER_OF_DAYS):
-            USERS_LOGINS[email] = last_login_obj, delta
-        else:
-            pass
+with open('OktaPasswordHealth.csv') as f:
+    F_CSV = csv.reader(f)
+    HEADERS = [re.sub('[^a-zA-Z_]', '_', h) for h in next(F_CSV)]
+    ROW = namedtuple('Row', HEADERS)
+    for rows in F_CSV:
+        row = ROW(*rows)
+        date = datetime.strptime(row.Last_Login, '%Y-%m-%d %H:%M:%S.%f')
+        last_login = date.date()
+        days_since_last_login = (today - last_login).days
+        DAYS_SINCE_LAST_LOGIN[row.User] = days_since_last_login
 
-# if the dictionary populated above has any values, print them
-# if not, alert user
-if USERS_LOGINS:
-    print(RTN())
-    for user, login in USERS_LOGINS.items():
-        print(f"user: {user}")
-        print(f"last login: {login[0].date()}")
-        print(f"days since last login: {login[1].days}")
-        print(RTN())
-else:
-    print("Each user has logged in within the number of days you specified.")
+print(RTN())
+print('user, days since last login')
+for user, days_since_last_login in sorted(DAYS_SINCE_LAST_LOGIN.items(),
+                                          key=lambda x: x[1], reverse=True):
+    print(user, days_since_last_login)
 
-write_to_csv('output_file.csv', USERS_LOGINS)
-print("'output_file.csv' exported successfully")
-    for k, v in USERS_LOGINS.items():
-        print('user:', k)
-        print('last login:', v[0].date())
-        print('days since last login:', v[1].days)
-        print(RTN())
+with open('days_since_last_login.csv', 'w') as out_file:
+    out_csv = csv.writer(out_file)
+    out_csv.writerow(['user','days_since_last_login'])
+    for user, days_since_last_login in DAYS_SINCE_LAST_LOGIN.items():
+        keys_values = (user, days_since_last_login)
+        out_csv.writerow(keys_values)
+
+print(RTN())
+print('"days_since_last_login.csv" exported successfully')
+print(RTN())
